114b83b699ba45c89e9ed0568ef838c666d27687
pawankachhap <pawankkachhap3@gmail.com>
2018-09-16 02:15:43 +0530

    trying different environments

---
+++ HCFandLCM.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main()
+{
+	int n1,n2,H=1,L=1,i,max;
+	scanf("%d %d",&n1,&n2);
+
+	//making n2<n1
+	if(n1<n2)
+	{
+		n1=n1+n2;
+		n2=n1-n2;
+		n1=n1-n2;
+	}
+	max=n1;
+
+	for(i=1;i<=n2;i++)
+	{
+		if(n1%i==0 && n2%i==0)
+		{
+			H*=i;
+		}
+	}
+	if(n1%n2==0)
+		L=n1;
+	else
+	{
+		while(1)
+		{
+			max++;
+			if(max%n1==0 && max%n2==0)
+			{
+				L=max;
+				break;
+			}
+		}
+	}
+	printf("HCF=%d\nLCM=%d",H,L);
+	return 0;
+}
