a67a748b1517e71ab667e25084c59d02a075e9cc
pawankachhap <pawankkachhap3@gmail.com>
2018-09-15 19:18:33 +0530

    first commit

---
+++ magic-number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int magic(int n)
+{
+    int temp=n,sum=0;
+    while(temp>=1)
+    {
+        if(temp<10)
+            sum+=temp*temp;
+        else
+        sum+=(temp%10)*(temp%10);
+
+        temp/=10;
+    }
+    if(sum%10==0 || sum==1)
+        return 1;
+    else if(sum<=n)
+        return -1;
+    else
+        return magic(sum);
+}
+
+//driving function'
+
+int main()
+{
+    int num=0,result=-1;
+    while(result!=1)
+    {
+        result=magic(++num);
+    }
+    printf("%d",num);
+}
